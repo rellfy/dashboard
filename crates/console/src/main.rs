a638dabe363e2f30b559d8646e7f3d6e9d0fc333
Lorenzo Fontoura <lorenzo@nioel.com>
2022-02-17 16:45:59 +1000

    create get_authorisation_code function

--- crates/console/src/main.rs
+++ crates/console/src/main.rs
@@ -1,5 +1,52 @@
 use dashboard;
 
+const SCOPE_STR: &'static str = "offline_access%20user.read%20mail.readwrite%20calendars.readwrite";
+
+/*
+    Azure app client id:
+    5d2e90a4-2356-4edc-ae81-80fcd4641575
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let authorisation_code = get_authorisation_code();
+}
+
+fn get_authorisation_code() -> String {
+    let register_app_txt: &str = "Register Azure app @ \
+        https://docs.microsoft.com/en-us/graph/auth-register-app-v2";
+    println!("Register app at {}", register_app_txt);
+    println!("Enter Azure app client ID:");
+    let mut client_id = String::new();
+    std::io::stdin().read_line(&mut client_id);
+    client_id = {
+        let mut chars = client_id.chars();
+        chars.next_back();
+        chars.as_str().to_owned()
+    };
+    let auth_url = format!(
+        "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?\
+        client_id={}\
+        &response_type=code\
+        &redirect_uri=http%3A%2F%2Flocalhost%3A6767\
+        &scope={}",
+        &client_id,
+        SCOPE_STR
+    );
+    println!("Visit the following URL and copy the redirect URL: {}", auth_url);
+    println!("Enter redirect URL:");
+    let mut redirect_url = String::new();
+    std::io::stdin().read_line(&mut redirect_url);
+    // Redirect URL should be in the format {}?code={}
+    let code: String = {
+        let mut split = redirect_url.split("?code=");
+        if split.clone().count() != 2 {
+            panic!("Invalid redirect URL. \
+                It should be in the format https://localhost:port?code={}");
+        }
+        split.next();
+        let mut chars = split.next().unwrap().chars();
+        chars.next_back();
+        chars.as_str().to_owned()
+    };
+    return code
 }
